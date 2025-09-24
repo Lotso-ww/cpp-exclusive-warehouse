708064d21c894f50a0e151728edc481a56b1e59e
huang-qiruiqq <3208137151@qq.com>
2025-09-25 07:05:54 +0800

    优选算法--滑动窗口\优选算法 --滑动窗口 11最大连续1的个数\优选算法 --滑动窗口 11最大连续1的个数\test.cpp

---
+++ 优选算法--滑动窗口/优选算法 --滑动窗口 11最大连续1的个数/优选算法 --滑动窗口 11最大连续1的个数/test.cpp
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int n = nums.size();
+        int left = 0, right = 0, len = 0, zero = 0;
+        while (right < n)
+        {
+            if (nums[right] == 0) zero++;
+            while (zero > k)
+            {
+                if (nums[left++] == 0) zero--;
+            }
+            len = max(len, right - left + 1);
+            right++;
+        }
+        return len;
+    }
+};
