143131266f650d8d4510402cbddb3dd1634bb964
huang-qiruiqq <3208137151@qq.com>
2025-10-17 07:42:58 +0800

    stack&&queue的实现\stack&&queue的实现\test.cpp

--- stack&&queue的实现/stack&&queue的实现/test.cpp
+++ stack&&queue的实现/stack&&queue的实现/test.cpp
@@ -1,5 +1,6 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS 1
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
 
@@ -25,25 +26,167 @@ using namespace std;
 //	return 0;
 //}
 
-#include"queue.h"
+//#include"queue.h"
+//
+//int main()
+//{
+//	//Lotso::queue<int> q;//默认是用的deque
+//	//Lotso::queue<int, vector<int>> q; // ֧这个不用最好
+//	Lotso::queue<int, list<int>> q;//链表实现
+//
+//	q.push(1);
+//	q.push(2);
+//	q.push(3);
+//	q.push(4);
+//
+//	while (!q.empty())
+//	{
+//		cout << q.front() << " ";
+//		q.pop();
+//	}
+//	cout << endl;
+//
+//	return 0;
+//}
+
+//#include<deque>
+//
+//// 与程序员相关的CPU缓存知识
+//// https://coolshell.cn/articles/20793.html
+//
+//int main()
+//{
+//	deque<int> dp;
+//	dp.push_back(1);
+//	dp.push_back(1);
+//	dp.push_back(1);
+//	dp.push_front(2);
+//	dp.push_front(3);
+//	dp.push_front(4);
+//
+//	dp[0] += 10;
+//	for (auto e : dp)
+//	{
+//		cout << e << " ";
+//	}
+//	cout << endl;
+//
+//	return 0;
+//}
+
+//#include<deque>
+//#include<vector>
+//void test_op1()
+//{
+//	srand(time(0));
+//	const int N = 1000000;
+//
+//	deque<int> dq;
+//	vector<int> v;
+//	for (int i = 0; i < N; ++i)
+//	{
+//		auto e = rand() + i;
+//		v.push_back(e);
+//		dq.push_back(e);
+//	}
+//
+//	int begin1 = clock();
+//	sort(v.begin(), v.end());
+//	int end1 = clock();
+//
+//	int begin2 = clock();
+//	sort(dq.begin(), dq.end());
+//	int end2 = clock();
+//
+//	printf("vector:%d\n", end1 - begin1);
+//	printf("deque:%d\n", end2 - begin2);
+//}
+////
+//void test_op2()
+//{
+//	srand(time(0));
+//	const int N = 1000000;
+//
+//	deque<int> dq1;
+//	deque<int> dq2;
+//
+//	for (int i = 0; i < N; ++i)
+//	{
+//		auto e = rand() + i;
+//		dq1.push_back(e);
+//		dq2.push_back(e);
+//	}
+//
+//	int begin1 = clock();
+//	sort(dq1.begin(), dq1.end());
+//	int end1 = clock();
+//
+//	int begin2 = clock();
+//	// 拷贝到vector
+//	vector<int> v(dq2.begin(), dq2.end());
+//	sort(v.begin(), v.end());
+//	dq2.assign(v.begin(), v.end());
+//	int end2 = clock();
+//
+//	printf("deque sort:%d\n", end1 - begin1);
+//	printf("deque copy vector sort, copy back deque:%d\n", end2 - begin2);
+//}
+//
+//int main()
+//{
+//	//test_op1();
+//	test_op2();
+//
+//	return 0;
+//}
+
+//********************************************************************************************
+
+// 仿函数/函数对象   对象可以像函数一样使用
+//template <class T> 
+//struct Less
+//{
+//	bool operator() (const T& x, const T& y) const { return x < y; }
+//};
+//#include<queue>
+//int main()
+//{
+//	//priority_queue<int> pq;//默认是大的优先级高(大堆)
+//	priority_queue<int,vector<int>,greater<int>> pq;//调整成默认是小的优先级高(小堆)
+//	pq.push(3);
+//	pq.push(1);
+//	pq.push(2);
+//	pq.push(4);
+//	pq.push(6);
+//
+//	while (!pq.empty())
+//	{
+//		cout << pq.top() << " ";
+//		pq.pop();
+//	}
+//
+//	return 0;
+//}
+
+//**********************************************************************************
+//priority_queue,实现部分测试
+#include "priority_queue.h"
 
 int main()
 {
-	//Lotso::queue<int> q;//默认是用的deque
-	//Lotso::queue<int, vector<int>> q; // ֧这个不用最好
-	Lotso::queue<int, list<int>> q;//链表实现
+	//Lotso::priority_queue<int> pq;
+	int a[] = { 30,4,2,66,3 };
+	Lotso::priority_queue<int> pq(a, a + 5);
+	pq.push(3);
+	pq.push(1);
+	pq.push(5);
+	pq.push(7);
+	pq.push(2);
 
-	q.push(1);
-	q.push(2);
-	q.push(3);
-	q.push(4);
-
-	while (!q.empty())
+	while (!pq.empty())
 	{
-		cout << q.front() << " ";
-		q.pop();
+		cout << pq.top() << " ";
+		pq.pop();
 	}
 	cout << endl;
-
-	return 0;
 }
