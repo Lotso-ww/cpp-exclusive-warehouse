292c87022065032864272126063c1599050aa707
huang-qiruiqq <3208137151@qq.com>
2025-10-18 07:12:17 +0800

    优选算法--二分查找 19.x的平方根

--- stack&&queue的实现/stack&&queue的实现/test.cpp
+++ stack&&queue的实现/stack&&queue的实现/test.cpp
@@ -51,9 +51,6 @@ using namespace std;
 
 //#include<deque>
 //
-//// 与程序员相关的CPU缓存知识
-//// https://coolshell.cn/articles/20793.html
-//
 //int main()
 //{
 //	deque<int> dp;
@@ -134,7 +131,7 @@ using namespace std;
 //
 //int main()
 //{
-//	//test_op1();
+//	test_op1();
 //	test_op2();
 //
 //	return 0;
@@ -143,50 +140,50 @@ using namespace std;
 //********************************************************************************************
 
 // 仿函数/函数对象   对象可以像函数一样使用
-//template <class T> 
-//struct Less
-//{
-//	bool operator() (const T& x, const T& y) const { return x < y; }
-//};
-//#include<queue>
-//int main()
-//{
-//	//priority_queue<int> pq;//默认是大的优先级高(大堆)
-//	priority_queue<int,vector<int>,greater<int>> pq;//调整成默认是小的优先级高(小堆)
-//	pq.push(3);
-//	pq.push(1);
-//	pq.push(2);
-//	pq.push(4);
-//	pq.push(6);
-//
-//	while (!pq.empty())
-//	{
-//		cout << pq.top() << " ";
-//		pq.pop();
-//	}
-//
-//	return 0;
-//}
-
-//**********************************************************************************
-//priority_queue,实现部分测试
-#include "priority_queue.h"
-
+template <class T> 
+struct Less
+{
+	bool operator() (const T& x, const T& y) const { return x < y; }
+};
+#include<queue>
 int main()
 {
-	//Lotso::priority_queue<int> pq;
-	int a[] = { 30,4,2,66,3 };
-	Lotso::priority_queue<int> pq(a, a + 5);
+	//priority_queue<int> pq;//默认是大的优先级高(大堆)
+	priority_queue<int,vector<int>,greater<int>> pq;//调整成默认是小的优先级高(小堆)
 	pq.push(3);
 	pq.push(1);
-	pq.push(5);
-	pq.push(7);
 	pq.push(2);
+	pq.push(4);
+	pq.push(6);
 
 	while (!pq.empty())
 	{
 		cout << pq.top() << " ";
 		pq.pop();
 	}
-	cout << endl;
+
+	return 0;
 }
+
+//**********************************************************************************
+//priority_queue,实现部分测试
+//#include "priority_queue.h"
+//
+//int main()
+//{
+//	//Lotso::priority_queue<int> pq;
+//	int a[] = { 30,4,2,66,3 };
+//	Lotso::priority_queue<int> pq(a, a + 5);
+//	pq.push(3);
+//	pq.push(1);
+//	pq.push(5);
+//	pq.push(7);
+//	pq.push(2);
+//
+//	while (!pq.empty())
+//	{
+//		cout << pq.top() << " ";
+//		pq.pop();
+//	}
+//	cout << endl;
+//}
