622d1ce8a97cf56951a819289da0543bd854bd76
huang-qiruiqq <3208137151@qq.com>
2025-10-22 12:37:03 +0800

    继承

--- 继承/继承/test.cpp
+++ 继承/继承/test.cpp
@@ -3,69 +3,74 @@
 #include<vector>
 #include<list>
 using namespace std;
-
-
-//基类/父类
-class Person
-{
-// 都可以用
-public:
-	//进入校园/图书馆/实验室刷二维码等身份认证
-	void identity()
-	{
-		cout << "void identity():" << _name << endl;
-	}
-	void func()
-	{
-		cout << _age << endl;
-	}
-
-//基类的保护成员可以直接在派生类中使用，但不能在类外使用
-protected:
-	string _name = "赵四";//姓名
-	string _address;//地址
-	string _tel;//电话
-
-//基类的私有成员无法在派生类中直接使用
-private:
-	int _age = 18;//年龄
-};
-
-//class类的话不写默认是私有继承，struct是公有继承
-// class Student:Person
-class Student : public Person
-{
-public:
-	//学习
-	void study()
-	{
-		//……
-		//基类私有成员(爸爸的私房钱)，派生类	中不可见，语法限制上不能直接使用
-		//cout << _age << endl;
-		
-		//但是可以间接使用
-		func();
-	}
-protected://在继承中保护用的比较多
-	int _stuid;//学号
-};
-class Teacher : public Person
-{
-public:
-	//授课
-	void teaching()
-	{
-		//…………
-	}
-protected:
-	string title;//职称
-};
+//
+//
+////基类/父类
+//class Person
+//{
+//// 公共成员：子类和类外都能访问
+//public:
+//	//进入校园/图书馆/实验室刷二维码等身份认证
+//	void identity()
+//	{
+//		cout << "void identity():" << _name << endl;
+//	}
+//	void func()
+//	{
+//		cout << _age << endl;
+//	}
+//
+//// 保护成员：子类能访问，类外不能访问（专门为继承设计）
+//protected:
+//	string _name = "赵四";//姓名
+//	string _address;//地址
+//	string _tel;//电话
+//
+//// 私有成员：子类和类外都不能直接访问（像“爸爸的私房钱”）
+//private:
+//	int _age = 18;//年龄
+//};
+//
+//// 子类Student：公有继承Person
+////class的话不写默认是私有继承，struct是公有继承
+//// class Student:Person
+//class Student : public Person
+//{
+//public:
+//	//学习
+//	void study()
+//	{
+//		//……
+//		//基类私有成员(爸爸的私房钱)，派生类	中不可见，语法限制上不能直接使用
+//		//cout << _age << endl;
+//		
+//		//父类公有函数能间接访问私有成员
+//		func();
+//	}
+//protected://在继承中保护用的比较多
+//	int _stuid;//学号
+//};
+//
+//// 子类Teacher：公有继承Person
+//class Teacher : public Person
+//{
+//public:
+//	//授课
+//	void teaching()
+//	{
+//		//…………
+//	}
+//protected:
+//	string title;//职称
+//};
+//
+//// 测试：子类能直接用父类的函数
 //int main()
 //{
 //	Student s;
 //	Teacher t;
-//	s.identity();
-//	s.study();
+//	s.identity();// 用父类的identity，输出“赵四”
+//	s.study();// 用子类的study，调用父类的func,输出了18
 //
 //	return 0;
 //}
@@ -150,14 +155,17 @@ protected:
 //	//上面的转换在之前类和对象以及其它的一些地方都讲到过，这里就不说了,主要是对比
 //
 //	Student sobj;
-//	// 1.派生类对象可以赋值给基类的指针/引用，这里跟前面不太一样
-//	Person* pp = &sobj;
-//	Person& rp = sobj;
+//	// 1. 子类对象 → 父类指针/引用（隐式转换，安全）
+//	Person* pp = &sobj;// 父类指针指向子类对象的“父类部分”
+//	Person& rp = sobj;// 父类引用引用子类对象的“父类部分”
+//
+//	// 2. 子类对象 → 父类对象（调用父类拷贝构造，只拷贝父类部分）
 //	// 派生类对象可以赋值给基类的对象是通过调用后面会讲解的基类的拷⻉构造完成的
 //	Person pobj = sobj;
-//	//2.基类对象不能赋值给派生类对象，这里会编译报错
-//	//sobj = pobj;
-//	//sobj = (Student)pobj;//这样也不行
+//
+//	//3. 父类对象 → 子类对象（编译报错，不安全）
+//	//sobj = pobj;//错误：父类没有子类的成员（比如学号）
+//	//sobj = (Student)pobj;// 强制转换也不行
 //
 //	return 0;
 //}
@@ -166,7 +174,7 @@ protected:
 //{
 //protected:
 //	string _name = "小李子"; // 姓名
-//	int _num = 111; // 身份证号
+//	int _num = 111; // 父类的_num：身份证号
 //};
 //
 //class Student : public Person
@@ -174,9 +182,12 @@ protected:
 //public:
 //	void Print()
 //	{
-//		//同名成员构成隐藏，只在乎是否同名，参数什么的都不用管
-//		cout << _num << endl;//999
-//		cout << Person::_num << endl;//可以类名::_num
+//		// 同名变量：默认访问子类的_num（学号）
+//		// 同名成员构成隐藏，只在乎是否同名，参数什么的都不用管
+//		cout << "子类的_num：" << _num << endl;//输出999
+//
+//		// 想访问父类的_num：必须加“父类::”
+//		cout << "父类的_num："<<Person::_num << endl;//输出111
 //	}
 //protected:
 //	int _num = 999; // 学号
@@ -187,11 +198,12 @@ protected:
 //	Student s;
 //	s.Print();
 //}
-//
+
 
 //class A
 //{
 //public:
+//	// 父类无参函数
 //	void fun()
 //	{
 //		cout << "func()" << endl;
@@ -200,7 +212,7 @@ protected:
 //class B : public A
 //{
 //public:
-//	//有同名成员构成隐藏，不管参数
+//	// 子类有参函数：函数名和父类相同，构成隐藏
 //	void fun(int i)
 //	{
 //		cout << "func(int i)" << i << endl;
@@ -210,101 +222,110 @@ protected:
 //int main()
 //{
 //	B b;
-//	b.fun(10);
-//	//b.fun();//这样不行，因为A里面那个无参的func被隐藏了，所以这里如果不带参数的话也调用不了b里面的。
-//	//b.A::fun();//可以这样写。
-//
+//	b.fun(10); // 调用子类的fun(int)，输出“func(int i)10”
+//	// b.fun(); // 编译报错：父类的fun()被隐藏了，不能直接调用
+//	b.A::fun(); // 想调用父类的fun()：加“父类::”，输出“func()”
 //	return 0;
 //}
 
 
-//class Person
-//{
-//public:
-//	Person(const char* name)
-//		:_name()
-//	{
-//		cout << "Person()" << endl;
-//	}
-//
-//	Person(const Person& p)//传的是派生类的话也可以转换，前面讲过
-//		:_name(p._name)
-//	{
-//		cout << "Person(const Person& p)" << endl;
-//	}
-//
-//	Person& operator=(const Person& p)
-//	{
-//		cout << "Person operator=(const Person& p)" << endl;
-//		if (this != &p)
-//			_name = p._name;
-//
-//		return *this;
-//	}
-//
-//	~Person()
-//	{
-//		cout << "~Person()" << endl;
-//	}
-//protected:
-//	string _name;//姓名
-//
-//};
+class Person
+{
+public:
+	// 父类带参构造（无默认构造）
+	Person(const char* name)
+		:_name()// 初始化父类的_name
+	{
+		cout << "Person()" << endl;
+	}
+
+	// 父类拷贝构造
+	Person(const Person& p)//传的是派生类的话也可以转换，前面讲过
+		:_name(p._name)
+	{
+		cout << "Person(const Person& p)" << endl;
+	}
+
+	Person& operator=(const Person& p)
+	{
+		cout << "Person operator=(const Person& p)" << endl;
+		if (this != &p)
+			_name = p._name;
+
+		return *this;
+	}
+
+	~Person()
+	{
+		cout << "~Person()" << endl;
+	}
+protected:
+	string _name;//姓名
+
+};
 
 
 //其实这里和前面学的默认成员函数规则差别也不大，还是想明白下面两点
 // 1.我们不写，默认生成的函数行为是什么?是否符合需求
 // 2.不符合，我们要自己实现，如何实现？
 
-//class Student :public Person
-//{
-//public:
-//	Student(const char* name = "赵四",int num = 18, const char* address = "西安")
-//		:Person(name)//这里可以显示写一下(不能直接用_name)，其实就把他当成一个自定义类型成员变量就可以了。基类的成员当成一个整体。
-//		,_num(num)
-//		,_address(address)
-//	{
-//		cout << "Student()" << endl;
-//	}
-//
-//	//这里其实只用显示写一下构造就行，拷贝，赋值重载，析构这里因为不涉及资源需要深拷贝和手动释放，默认生成的就行
-//	//但是我们还是写出来感受一下
-//
-//	Student(const Student& s)
-//		:Person(s)//和上面构造一样，这里传s到Person的拷贝构造也是可以的，之前讲过
-//		,_num(s._num)
-//		,_address(s._address)
-//	{
-//		//涉及深拷贝的话就需要自己手动处理了
-//	}
-//
-//	Student& operator=(const Student& s)
-//	{
-//		if (this != &s)
-//		{
-//			Person::operator=(s);
-//			_num = s._num;
-//			_address = s._address;
-//		}
-//
-//		//涉及深拷贝，需要自己实现
-//		return *this;
-//	}
-//	~Student()
-//	{
-//		//不用显示调用基类析构，编译器会在派生类析构结束后自动调用析构
-//		//Person::~Person();//前面也需要Person::~Person，有一定历史原因，有提过
-//		// 再就是这里如果显示调用无法保证析构先子后父的原则，构造是先父后子
-//		// 先父后子：我们联想一下之前初始化列表按声明顺序来的原理
-//		// 先子后父：我们可以想一下如果先析构父类，那么子类的成员如果需要访问父类就出问题了
-//		//………………
-//		cout << "~Student()" << endl;
-//	}
-//protected:
-//	int _num;		 // 学号
-//	string _address; // 地址
-//	// int* _ptr;
-//};
+class Student :public Person
+{
+public:
+	// 子类构造：必须在初始化列表显式调用父类构造
+	Student(const char* name = "赵四",int num = 18, const char* address = "西安")
+		//这里可以显示写一下(不能直接用_name)，其实就把他当成一个自定义类型成员变量就可以了。基类的成员当成一个整体。
+		:Person(name)// 先初始化父类（必须写在前面）
+		,_num(num)// 再初始化子类自己的成员
+		,_address(address)
+	{
+		cout << "Student()" << endl;
+	}
+
+	//这里其实只用显示写一下构造就行，拷贝，赋值重载，析构这里因为不涉及资源需要深拷贝和手动释放，默认生成的就行
+	//但是我们还是写出来感受一下
+
+	// 子类拷贝构造
+	Student(const Student& s)
+		:Person(s)// 调用父类拷贝构造，拷贝父类部分（s是子类，能隐式转父类）
+		,_num(s._num)// 拷贝子类自己的学号
+		,_address(s._address)// 拷贝子类自己的地址
+	{
+		// 如果有深拷贝资源（比如int*），这里就要手动处理了。
+	}
+
+	Student& operator=(const Student& s)
+	{
+		if (this != &s)// 防止自己赋值自己
+		{
+			// 先赋值父类部分
+			Person::operator=(s);
+			// 再赋值子类自己的成员
+			_num = s._num;
+			_address = s._address;
+
+			// 如果有深拷贝资源在这里处理
+		}
+
+		return *this;
+	}
+	~Student()
+	{
+		//不用显示调用基类析构，编译器会在派生类析构结束后自动调用析构
+		//Person::~Person();//前面也需要Person::~Person，有一定历史原因，有提过
+		// 再就是这里如果显示调用无法保证析构先子后父的原则，构造是先父后子
+		// 先父后子：我们联想一下之前初始化列表按声明顺序来的原理
+		// 先子后父：我们可以想一下如果先析构父类，那么子类的成员如果需要访问父类就出问题了
+		//………………
+		// 先执行子类析构逻辑（比如释放子类的资源）
+		cout << "~Student()" << endl;
+		// 编译器自动调用父类析构：~Person()
+	}
+protected:
+	int _num;		 // 学号
+	string _address; // 地址
+	// int* _ptr;
+};
 //
 ////构造
 ////继承的基成员变量(整体对象)+自己的成员变量(遵循普通的规则，跟类和对象部分一样)
@@ -314,15 +335,27 @@ protected:
 //
 ////派生类一般要自己实现构造，不需要显示写析构，构造函数，赋值重载，除法派生类有深拷贝的资源需要处理
 //
+int main()
+{
+ 	// 构造顺序：先Person(),再Student()
+	// 先父后子：我们联想一下之前初始化列表按声明顺序来的原理
+	// 析构顺序：先~Student()，再~Person()
+	// 先子后父：我们可以想一下如果先析构父类，那么子类的成员如果需要访问父类就出问题了
+	Student s1;
+	//Student s2("小明",10);
+	//Student s3(s2);
+
+	////可以直接这样用
+	//s1 = s3;
+	//Person p = s1;//可以直接这样用
+
+	return 0;
+}
+
 //int main()
 //{
-//	Student s1;
-//	//Student s2("小明",10);
-//	//Student s3(s2);
-//
-//	////可以直接这样用
-//	//s1 = s3;
-//	//Person p = s1;//可以直接这样用
-//
+//	
+//	// 构造顺序：先调用Person(name)，再调用Student()
+//	Student s1("张三", 20, "北京");
 //	return 0;
 //}
