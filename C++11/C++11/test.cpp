65b19533d5ee36d6d8f3f3ac6911a4e1834b62f9
@huang-qiruiqq <3208137151@qq.com>
2025-11-15 21:39:02 +0800

    C++11\C++11\test.cpp

--- C++11/C++11/test.cpp
+++ C++11/C++11/test.cpp
@@ -238,34 +238,34 @@ using namespace std;
 //}
 
 
-void f(int& x)
-{
-	std::cout << "左值引用重载 f(" << x << ")\n";
-}
-void f(const int& x)
-{
-	std::cout << "到 const 的左值引用重载 f(" << x << ")\n";
-}
-void f(int&& x)
-{
-	std::cout << "右值引用重载 f(" << x << ")\n";
-}
-int main()
-{
-	int i = 1;
-	const int ci = 2;
-	f(i); // 调用 f(int&)
-	f(ci); // 调用 f(const int&)
-	f(3); // 调用 f(int&&)，如果没有 f(int&&) 重载则会调用 f(const int&)
-	f(std::move(i)); // 调用 f(int&&)
-
-
-	// 右值引用变量在用于表达式时是左值
-	int&& x = 1;
-	f(x); // 调用 f(int& x)
-	f(std::move(x)); // 调用 f(int&& x)
-	return 0;
-}
+//void f(int& x)
+//{
+//	std::cout << "左值引用重载 f(" << x << ")\n";
+//}
+//void f(const int& x)
+//{
+//	std::cout << "到 const 的左值引用重载 f(" << x << ")\n";
+//}
+//void f(int&& x)
+//{
+//	std::cout << "右值引用重载 f(" << x << ")\n";
+//}
+//int main()
+//{
+//	int i = 1;
+//	const int ci = 2;
+//	f(i); // 调用 f(int&)
+//	f(ci); // 调用 f(const int&)
+//	f(3); // 调用 f(int&&)，如果没有 f(int&&) 重载则会调用 f(const int&)
+//	f(std::move(i)); // 调用 f(int&&)
+//
+//
+//	 右值引用变量在用于表达式时是左值
+//	int&& x = 1;
+//	f(x); // 调用 f(int& x)
+//	f(std::move(x)); // 调用 f(int&& x)
+//	return 0;
+//}
 
 
 //#define _CRT_SECURE_NO_WARNINGS 1
@@ -449,18 +449,21 @@ int main()
 //    }
 //}
 
-
+//// 场景一
 //int main()
 //{
 //    Lotso::string ret = Lotso::addStrings("11111", "2222");
+//    cout << ret.c_str() << endl;
 //    cout << &ret << endl;
 //}
+//
+//// 场景二：
 //int main()
 //{
 //    Lotso::string ret;
 //    // ……
 //    ret = Lotso::addStrings("11111", "2222");
-//
+//    cout << ret.c_str() << endl;
 //    cout << &ret << endl;
 //}
 
@@ -488,3 +491,131 @@ int main()
 //
 //    return 0;
 //}
+
+
+
+// List.h
+// 以下代码把跟这里无关的接口都删除了
+namespace Lotso
+{
+	template<class T>
+	struct ListNode
+	{
+		ListNode<T>* _next;
+		ListNode<T>* _prev;
+		T _data;
+		ListNode(const T& data = T())
+			:_next(nullptr)
+			, _prev(nullptr)
+			, _data(data)
+		{
+		}
+		ListNode(T&& data)
+			:_next(nullptr)
+			, _prev(nullptr)
+			, _data(move(data))
+		{
+		}
+	};
+	template<class T, class Ref, class Ptr>
+	struct ListIterator
+	{
+		typedef ListNode<T> Node;
+		typedef ListIterator<T, Ref, Ptr> Self;
+		Node* _node;
+		ListIterator(Node* node)
+			:_node(node)
+		{
+		}
+		Self& operator++()
+		{
+			_node = _node->_next;
+			return *this;
+		}
+		Ref operator*()
+		{
+			return _node->_data;
+		}
+		bool operator!=(const Self& it)
+		{
+			return _node != it._node;
+		}
+	};
+	template<class T>
+	class list
+	{
+		typedef ListNode<T> Node;
+	public:
+		typedef ListIterator<T, T&, T*> iterator;
+		typedef ListIterator<T, const T&, const T*> const_iterator;
+		iterator begin()
+		{
+			return iterator(_head->_next);
+		}
+		iterator end()
+		{
+			return iterator(_head);
+		}
+		void empty_init()
+		{
+			_head = new Node();
+			_head->_next = _head;
+			_head->_prev = _head;
+		}
+		list()
+		{
+			empty_init();
+		}
+		void push_back(const T& x)
+		{
+			insert(end(), x);
+		}
+		void push_back(T&& x)
+		{
+			insert(end(), move(x));
+		}
+		iterator insert(iterator pos, const T& x)
+		{
+			Node* cur = pos._node;
+			Node* newnode = new Node(x);
+			Node* prev = cur->_prev;
+			// prev newnode cur
+			prev->_next = newnode;
+			newnode->_prev = prev;
+			newnode->_next = cur;
+			cur->_prev = newnode;
+			return iterator(newnode);
+		}
+		iterator insert(iterator pos, T && x)
+		{
+			Node* cur = pos._node;
+			Node* newnode = new Node(move(x));
+			Node* prev = cur->_prev;
+			// prev newnode cur
+			prev->_next = newnode;
+			newnode->_prev = prev;
+			newnode->_next = cur;
+			cur->_prev = newnode;
+			return iterator(newnode);
+		}
+	private:
+		Node* _head;
+	};
+}
+// Test.cpp
+#include"List.h"
+int main()
+{
+	Lotso::list<Lotso::string> lt;
+	cout << "*************************" << endl;
+	Lotso::string s1("111111111111111111111");
+	lt.push_back(s1);
+	cout << "*************************" << endl;
+	lt.push_back(Lotso::string("22222222222222222222222222222"));
+	cout << "*************************" << endl;
+	lt.push_back("3333333333333333333333333333");
+	cout << "*************************" << endl;
+	lt.push_back(move(s1));
+	cout << "*************************" << endl;
+	return 0;
+}
